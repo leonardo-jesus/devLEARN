db3b20d1d16a857657f04ee8bf477f9b8c11882f
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-01 16:05:24 -0300

    Update db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,2 +1,25 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb//localhost/test', {userNewUrlParser: true});
+mongoose.connect(process.env.URI, {userNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'conection error'));
+db.once('open', () => {
+  console.log('Connection successfull');
+});
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
