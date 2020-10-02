4b8177bdb91f6725085cbf201e12c05ec6e637be
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-02 18:22:06 -0300

    Add authentication to login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const authConfig = process.env.SECRET;
 
 const User = require('../models/User');
 
@@ -20,5 +26,25 @@ router.post('/register', async (req, res) => {
     return res.status(400).send({ 'msg': `Registration failed` });
   }
 });
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user)
+    return res.status(400).send({ 'msg': 'User not found' });
+  
+  if (!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ 'msg': 'Invalid password' });
+  
+  user.password = undefined;
+
+  const token = jwt.sign({ id: user.id }, authConfig, {
+    expiresIn: 86400,
+  });
+
+  res.send({ user, token });
+})
 
 module.exports = app => app.use('/auth', router);
