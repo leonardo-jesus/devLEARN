42c5e20ace9f730d33e0f2c3deae7c226643de66
leonardo-jesus <leonardojesus02@outlook.com>
2021-05-03 20:45:36 -0300

    refact: Change deprecated 'body-parser' to express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 require('./controllers/authController')(app);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`✔ Listening on port ${port}...`));
