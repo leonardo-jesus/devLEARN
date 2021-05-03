55c2f8926f3aba6e39ed29ccbb154c4a04523e99
leonardo-jesus <leonardojesus02@outlook.com>
2021-05-03 20:45:55 -0300

    refact: Remove unnecessary code

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,14 +1,5 @@
-require('dotenv').config();
-
 const mongoose = require('mongoose');
 mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
-
-((/* parameters */) => {
-  // code here
-})(/* parameters */);
-
-
-// URI = mongodb+srv://leonardo-jesus02:a1b2c3d4e5@devlearn-cluster.vlbqd.gcp.mongodb.net/devLEARN-db?retryWrites=true&w=majority
