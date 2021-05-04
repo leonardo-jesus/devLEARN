9a1b70a89e049c594c608fb85469daf072033e56
leonardo-jesus <leonardojesus02@outlook.com>
2021-05-03 21:29:27 -0300

    refact: Add brackets to IFs

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,42 +11,45 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-  const { email } = req.body;
+    const { email } = req.body;
 
-  try {
-    if (await User.findOne({ email }))
-      return res.status(400).send({ 'msg': 'User already exists'});
+    try {
+        if (await User.findOne({ email })) {
+            return res.status(400).send({ 'msg': 'User already exists' });
+        }
 
-    const user = await User.create(req.body);
-    
-    user.password = undefined;
+        const user = await User.create(req.body);
 
-    return res.send(user);
-  } catch (e) {
-    return res.status(400).send({ 'msg': `Registration failed` });
-  }
+        user.password = undefined;
+
+        return res.send(user);
+    } catch (e) {
+        return res.status(400).send({ 'msg': `Registration failed` });
+    }
 });
 
 router.post('/authenticate', async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email }).select('+password');
+    const user = await User.findOne({ email }).select('+password');
 
-  if (!user)
-    return res.status(400).send({ 'msg': 'User not found' });
-  
-  if (!await bcrypt.compare(password, user.password))
-    return res.status(400).send({ 'msg': 'Invalid password' });
-  
-  user.password = undefined;
+    if (!user) {
+        return res.status(400).send({ 'msg': 'User not found' });
+    }
 
-  const twentyFourHours = 86400;
+    if (!await bcrypt.compare(password, user.password)) {
+        return res.status(400).send({ 'msg': 'Invalid password' });
+    }
 
-  const token = jwt.sign({ id: user.id }, authConfig, {
-    expiresIn: twentyFourHours,
-  });
+    user.password = undefined;
 
-  res.send({ user, token });
-})
+    const twentyFourHours = 86400;
+
+    const token = jwt.sign({ id: user.id }, authConfig, {
+        expiresIn: twentyFourHours,
+    });
+
+    res.send({ user, token });
+});
 
 module.exports = app => app.use('/auth', router);
